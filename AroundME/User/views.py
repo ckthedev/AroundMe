9d2a84118955da3439eea63c58e87d9bd44b3288
Arjunck <107870147+ckthedev@users.noreply.github.com>
2023-02-27 16:04:53 +0530

    27/2/2023

--- AroundME/User/views.py
+++ AroundME/User/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import View,CreateView,TemplateView
+from .forms import BioForm
+from .models import Bio
+from django.urls import reverse_lazy
+from django.contrib import messages
 
 class UserHome(TemplateView):
     template_name="userhome.html"
@@ -7,3 +11,17 @@ class UserHome(TemplateView):
 
 class Profile(TemplateView):
     template_name="profile.html"    
+
+
+class Bio(CreateView):
+    form_class=BioForm
+    template_name="bio.html"
+    model=Bio
+    success_url=reverse_lazy("profile")
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        self.object = form.save()
+        messages.success(self,request,"Bio Added!")
+        return super().form_valid(form)
+        
+
